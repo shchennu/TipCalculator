7a8bdc43d50654e8541afe733d618f7543640866
sri harsha <chennu.sriharsha@gmail.com>
2014-11-11 18:05:46 +0000

    changed tip calculator

--- tip_calculator1.rb
+++ tip_calculator1.rb
@@ -1,15 +1,19 @@
-meal = 20.00
-tax  = 12.00 / 100
-tip  = 20.00 / 100
-
-tax_value      = tax*meal.to_f
-meal_with_tax  = meal+tax_value
-tip_value      = tip*meal.to_f
-total_cost     = meal_with_tax+tip_value
-
-
-
-puts("The pre-tax cost of your meal was $#{meal}")
-puts("At 12%, tax for this meal is $#{tax_value}.")
-puts("For a 20% tip, you should leave $#{tip_value}.")
-puts("The grand total for this meal is then $#{total_cost}.")
+# first we assign our initial variables
+meal_cost = 20.0
+tax_percent = 12
+tip_percent = 20
+ 
+# now we compute 
+tax_value = meal_cost * tax_percent/100
+meal_with_tax = meal_cost + tax_value
+tip_value = meal_with_tax * tip_percent/100
+total_cost = meal_with_tax + tip_value
+ 
+ 
+# note how we use string interpolation below. 
+# the %.2f notation tells Ruby to display our subbed in value (meal_cost) as a decimal with two decimal places.
+# You can read more about string formating in Ruby 2.0 here: http://ruby-doc.org/core-2.0/Kernel.html#method-i-sprintf
+print "The pre-tax cost of your meal was $%.2f.\n" % meal_cost
+print "At %d%%, tax for this meal is $%.2f.\n" % [tax_percent, tax_value]
+print "For a %d%% tip, you should leave $%.2f.\n" % [tip_percent, tip_value]
+print "The grand total for this meal is then $%.2f.\n" % total_cost
